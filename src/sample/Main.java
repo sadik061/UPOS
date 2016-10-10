bd60cdbb56a53bdb2ea5be4bf846334c1207df2f
Sadik Ahammed <sadik061@gmail.com>
2016-10-10 16:37:01 +0600

    hau

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,12 +8,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    static Stage window;
+
+    public static Stage getWindow() {
+        return window;
+    }
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        window= primaryStage;
+        window.setTitle("upos");
+        window.setScene(new Scene(root, 300, 275));
+        window.show();
     }
 
 
