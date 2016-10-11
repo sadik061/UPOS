6e31d94d555705e82c0d856556b4b737c1c974f6
abdullahalrifat <abdullahalrifat95@gmail.com>
2016-10-11 20:17:08 +0600

    Add Products Minor Update

--- src/sample/AddProductsController.java
+++ src/sample/AddProductsController.java
@@ -1,7 +1,37 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  * Created by abdullah on 10/11/16.
  */
-public class AddProductsController {
+public class AddProductsController
+{
+    @FXML
+    private Button generate;
+    @FXML
+    private Button barcode;
+    @FXML
+    private TextField productname;
+    @FXML
+    private TextField productprice;
+    @FXML
+    private TextField productcode;
+    @FXML
+    private TextField productquantity;
+    @FXML
+    public void initialize()
+    {
+
+    }
+    public void generateBarCode()
+    {
+
+    }
+    public void scanBarCode()
+    {
+
+    }
 }
